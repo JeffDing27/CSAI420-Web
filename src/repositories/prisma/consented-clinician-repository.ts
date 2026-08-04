1fcea93be9e0e7a8cf99c59c8882907750d71089
JeffDing27 <rud300@ensign.edu>
2026-08-04 12:16:47 -0600

    Add provider patient lookup API

--- src/repositories/prisma/consented-clinician-repository.ts
+++ src/repositories/prisma/consented-clinician-repository.ts
@@ -2,12 +2,18 @@ import type { ConsentedClinician } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 import type { ConsentedClinicianRepository } from "../interfaces";
 
-export class PrismaConsentedClinicianRepository
-  implements ConsentedClinicianRepository
-{
+export class PrismaConsentedClinicianRepository implements ConsentedClinicianRepository {
   async findByCustomer(customer: string): Promise<ConsentedClinician[]> {
     return prisma.consentedClinician.findMany({
       where: { customer },
+    });
+  }
+
+  async findByClinicianUsername(
+    clinicianUsername: string,
+  ): Promise<ConsentedClinician[]> {
+    return prisma.consentedClinician.findMany({
+      where: { clinicianUsername },
     });
   }
 
@@ -16,9 +22,17 @@ export class PrismaConsentedClinicianRepository
     clinicianUsername: string,
   ): Promise<ConsentedClinician> {
     return prisma.consentedClinician.upsert({
-      where: { customer_clinicianUsername: { customer, clinicianUsername } },
+      where: {
+        customer_clinicianUsername: {
+          customer,
+          clinicianUsername,
+        },
+      },
       update: {},
-      create: { customer, clinicianUsername },
+      create: {
+        customer,
+        clinicianUsername,
+      },
     });
   }
 }
